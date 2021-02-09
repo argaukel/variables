25584ae655c38e47f0ed45747abc41c6cec291bf
A Reilly Gaukel <45907561+argaukel@users.noreply.github.com>
2021-02-08 20:48:26 -0600

    loop through res length with object.keys

--- icecream/272021.js
+++ icecream/272021.js
@@ -17,5 +17,22 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  // connection.end();
+  afterConnection();
 });
+
+function afterConnection() {
+  connection.query("select * from products", function(err, res) {
+    if (err) throw err;
+    var length = Object.keys(res).length;
+    console.log(length);
+    console.table(res);
+    // console.log(res[1].flavor);
+    console.log("Flavor of item 0:",res[0].flavor);
+    // console.log(res);
+    for (var i = 0; i < length; i++){
+      console.log("Flavor of item ", i,": ", res[i].flavor)
+    };
+    connection.end();
+  });
+}
--- trial/272021.js
+++ trial/272021.js
@@ -1,3 +1,45 @@
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+
+//   // Your port; if not 3306
+//   port: 3306,
+
+//   // Your username
+//   user: "newuser",
+
+//   // Your password
+//   password: "password",
+//   database: "ice_creamDB"
+// });
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId);
+//   // connection.end();
+//   afterConnection();
+// });
+
+// // function afterConnection() {
+// //   // console.log("here");
+// //   connection.query("SELECT * FROM products", function(err, res) {
+// //     if (err) throw err;
+// //     console.table(res);
+// //     console.log("Flavor of item 0:",res[0].flavor)
+// //     connection.end();
+// //   });
+// // }
+
+// function afterConnection() {
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     console.table(res);
+//     console.log("Flavor of item 0:",res[0].flavor)
+//     connection.end();
+//   });
+// }
+
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -16,6 +58,17 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("here");
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  
+  afterConnection();
 });
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    console.log("Flavor of item 0:",res[0].flavor)
+    connection.end();
+  });
+}
