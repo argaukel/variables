4ef1facada7cf03ef4a2fcb371dc8506b6a1ef4e
A Reilly Gaukel <45907561+argaukel@users.noreply.github.com>
2021-02-11 21:28:39 -0600

    inquiry

--- 282021/playlist2.js
+++ 282021/playlist2.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection ({
 port: 3306,
@@ -11,12 +12,47 @@ database: "playlist_db"
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    // updateSongDynamic("Breakfast tea", "Eli Way");
-    queryAllSongs();
+    // insertSongDynamic("Sleepy Vibes", "The Deli", "lofi");
+    // updateSongDynamic("Redberry tea", "Eli Way");
+    // queryAllSongs();
     // queryByGenre("lofi");
     // updateSongs();
+    // insertSong();
+    // deleteSongDynamic(5);
+    // inquiry();
     connection.end();
 });
+
+// function inquiry() {
+    // var inquiry = inquirer
+    // .prompt([
+    //     {
+    //         type: 'checkbox',
+    //         message: 'Select Genre',
+    //         name: 'Genre',
+    //         choices: [
+    //             new inquirer.Separator(' The Genres '),
+    //             {
+    //               name: 'lofi',  
+    //             },
+    //             {
+    //                 name: "Chillhop",
+    //             },
+    //         ],
+    //         validate: function (answer) {
+    //             if (answer.length < 1) {
+    //                 return 'You gotta choose!';
+    //             }
+
+    //             return true;
+    //         },
+    //     },
+    // ])
+    // .then((answers) => {
+    //     console.log(JSON.stringify(answers, null, '  '));
+    // });
+//         console.log(inquiry);
+// }
 
 function queryAllSongs() {
     connection.query("SELECT * FROM songs", function(err, res) {
@@ -31,6 +67,52 @@ function queryByGenre(genre) {
         console.table(res)
     });
     console.log(query.sql);
+};
+
+function insertSong() {
+    var insertSongQuery = connection.query(
+        "INSERT INTO songs SET ?",
+        {
+            title: "Dreamscape",
+            artist: "deadxbeat",
+            genre: "chillhop"
+        },
+        function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            console.log(res.affectedRows + " product inserted!\n");
+        }
+    );
+    console.log(insertSongQuery.sql);
+};
+
+function deleteSongDynamic(id) {
+    var deleteQuery = connection.query(
+        "DELETE FROM songs WHERE ?",
+        {
+            id: id,
+        },
+        function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " products deleted!\n");
+        }
+    );
+    console.log(deleteQuery.sql);
+};
+
+function insertSongDynamic(title, artist, genre) {
+    var insertQuery = connection.query(
+        "INSERT INTO songs SET ?",
+            {
+                title: title,
+                artist: artist,
+                genre: genre,
+            },
+        function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " products updated!\n");
+        });
+    console.log(insertQuery.sql);
 }
 
 function updateSongDynamic(title, artist) {
@@ -45,7 +127,7 @@ function updateSongDynamic(title, artist) {
             }
         ],
         function(err, res) {
-                if (err) throw err,
+                if (err) throw err;
                 console.log(res.affectedRows + " products updated!\n");
         }
     );
