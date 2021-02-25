68d84a22b1f04ca6d84445e8c6489d00395e239e
A Reilly Gaukel <45907561+argaukel@users.noreply.github.com>
2021-02-25 10:18:55 -0600

    error handling

--- 282021/inquiry.js
+++ 282021/inquiry.js
@@ -30,18 +30,35 @@ var artistPrompt = {
     type: 'input',
     name: 'Artist',
     message: "Who's the artist?",
+    validate: function (value) {
+        if (value == "") {
+            console.log("c'mon now!")
+        } else {
+            return true;
+        }
+    }
 };
 
-inquirer.prompt(titlePrompt).then(answers => {
-    console.log(answers.title);
-    
+var questions = [
+    genrePrompt,
+    titlePrompt,
+    artistPrompt,
+
+]
+
+function runPrompts() {
+inquirer.prompt(questions).then(answers => {
+    // console.log(answers.title);
+    console.log(answers);
     // console.log(answers.title);
     // if (answers.title = " ") {
     //     console.log("rude")
     // }
 });
 
+};
 
+runPrompts()
 
 
 
---
+++ 282021/onequestionsvar.js
@@ -0,0 +1,12 @@
+var inquirer = require("inquirer");
+
+var questions = [
+    console.log("test 1")
+    
+]
+
+function runQuestions() {
+    questions
+}
+
+runQuestions();
